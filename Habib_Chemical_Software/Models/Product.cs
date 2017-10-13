8df5d6bfa152e419cf36448eb196f58d16e8a011
Hasbulla <hasbullakhan20@gmail.com>
2017-10-13 10:15:04 +0600

    Added Product Model

--- Habib_Chemical_Software/Models/Product.cs
+++ Habib_Chemical_Software/Models/Product.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Habib_Chemical_Software.Models
+namespace Habib_Chemical_Software
 {
-    public class Product
+    public partial class ProductMetaData
+    {
+        [Display(Name = "Id")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Please Provide Product Name")]
+        [Display(Name = "Product Name")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Please Select a Category")]
+        [Display(Name = "Category")]
+        public int category_id { get; set; }
+        
+        [Display(Name = "Country")]
+        public string country { get; set; }
+        
+        [Display(Name = "Company Name")]
+        public string company { get; set; }
+
+        [Required(ErrorMessage = "Please Select Product Type")]
+        [Display(Name = "Product Type")]
+        public string product_type { get; set; }
+
+        [Required(ErrorMessage = "Please Select Weight Type")]
+        [Display(Name = "Weight Type")]
+        public int weight_type { get; set; }
+
+        [Required(ErrorMessage = "Please Provide Weight in Single Bag/Drum/Barrel")]
+        [Display(Name = "Weight Per Unit")]
+        public int weight_per_bag { get; set; }
+        
+        [Display(Name = "Description")]
+        public string description { get; set; }
+        
+        [DataType(DataType.Currency)]
+        [Display(Name = "Current Amount")]
+        public int current_amount { get; set; }
+
+        [Display(Name = "Photo")]
+        public string photo { get; set; }
+    }
+
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
     {
     }
 }
