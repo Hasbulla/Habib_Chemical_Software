ed5d3f41184327d8a76a2d16d73d4d4c7a4beb3b
Hasbulla <hasbullakhan20@gmail.com>
2017-10-13 09:57:49 +0600

    Added Product Business Object

---
+++ Habib_Chemical_Software/BO/ProductsBO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Habib_Chemical_Software.BO
+{
+    public class ProductsBO
+    {
+        Shared<Product> rep = new Shared<Product>();
+        public IEnumerable<Product> GetAll()
+        {
+            return rep.GetAll(c => c.deleted == false);
+        }
+        public Product GetById(int id)
+        {
+            return rep.GetById(id);
+        }
+        public Product Add(Product entity)
+        {
+            return rep.Add(entity);
+        }
+        public void Update(Product entity)
+        {
+            rep.Update(entity);
+        }
+        public void Delete(int id)
+        {
+            rep.Delete(id);
+        }
+        public void Dispose()
+        {
+            rep.Dispose();
+        }
+    }
+}
