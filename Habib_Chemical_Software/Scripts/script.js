f97fa9f66b8a50be0c49e50f77d2f038d6a4dc2b
Hasbulla <hasbullakhan20@gmail.com>
2017-10-18 00:58:22 +0600

    Working on Company Contact

--- Habib_Chemical_Software/Scripts/script.js
+++ Habib_Chemical_Software/Scripts/script.js
@@ -30,4 +30,49 @@ $(document).ready(function () {
     $('input[type=file]').change(function () {
         $("#productEditSpan").html($(this).val());
     });
+
+    var counter = 2;
+
+    $("#addContactTextBoxes").click(function () {
+
+        if (counter > 4) {
+            alert("Only 4 textboxes allow");
+            return false;
+        }
+
+        var addtext = '<div class="form-group"  id="contactGroup'+ counter +'"> ' +
+            '<div class="input-group"> ' +
+                '<span class="input-group-addon">Name</span> ' +
+            '<input type="text" name="name' + counter + '" id="name' + counter +'" class="form-control" /> ' +
+                '<span class="input-group-addon">Contact</span> ' +
+            '<input type="text" name="contact' + counter + '" id="contact' + counter +'" class="form-control" /> ' +
+                '</div > ' +
+            '</div>';
+
+        $("#contactGroup1").after(addtext);
+
+
+        counter++;
+    });
+
+    $("#removeButton").click(function () {
+        if (counter == 1) {
+            alert("No more textbox to remove");
+            return false;
+        }
+
+        counter--;
+
+        $("#TextBoxDiv" + counter).remove();
+
+    });
+
+    $("#getButtonValue").click(function () {
+
+        var msg = '';
+        for (i = 1; i < counter; i++) {
+            msg += "\n Textbox #" + i + " : " + $('#textbox' + i).val();
+        }
+        alert(msg);
+    });
 })
